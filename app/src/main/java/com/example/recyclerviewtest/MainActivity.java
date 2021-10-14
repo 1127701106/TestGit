3413b37811ea55e269387388ffa279cb5ab72861
1127701106 <1127701106@qq.com>
2021-10-14 13:51:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.recyclerviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public class TextItem {
+
+        private int ImageId;
+        private String textId;
+
+        public TextItem(int imageId, String textId) {
+            ImageId = imageId;
+            this.textId = textId;
+        }
+
+        public int getImageId() {
+            return ImageId;
+        }
+
+        public void setImageId(int imageId) {
+            ImageId = imageId;
+        }
+
+        public String getTextId() {
+            return textId;
+        }
+
+        public void setTextId(String textId) {
+            this.textId = textId;
+        }
+    }
+}
